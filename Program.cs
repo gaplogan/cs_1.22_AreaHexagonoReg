9b882df6faed596605fd36991ad6bc753d421041
Gabriel <gaplogan@gmail.com>
2021-04-10 20:46:50 -0300

    2021.04.10.20.46

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace cs_1._22_AreaHexagonoReg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lado, area;
+
+            // Validar o lado.
+            do
+            {
+                Console.Write("Digite o valor do lado do hexágono regular: ");
+                lado = double.Parse(Console.ReadLine());
+                lado = Math.Abs(lado);
+
+            } while (lado <= 0);
+
+            area = (3 * lado * lado * Math.Sqrt(3)) / 2;
+
+            Console.WriteLine($"\nÁrea do hexágono regular: {area}");
+        }
+    }
+}
